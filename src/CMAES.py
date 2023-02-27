122954d45c3a4c73e06b4f62d615f18b1273bf6b
Elian Mangin <elian.mangin@student.fr>
2023-02-27 17:08:28 +0100

    version fonctionnelle

--- src/CMAES.py
+++ src/CMAES.py
@@ -12,9 +12,11 @@ boudary = {'bounds': [0, [2.99, 2.99, max_threads//8,
                           max_thrdblock_x//8, max_thrdblock_y//8, max_thrdblock_z//8]]}
 
 
+param = {'olevels': '-O2', 'simds': 'avx', 'problem_size_x': 256, 'problem_size_y': 256,
+         'problem_size_z': 256, 'n_threads': 16, 'thrdblock_x': 16, 'thrdblock_y': 16, 'thrdblock_z': 16}
+
+
 def x_to_param(x):
-    param = {'olevels': '-O2', 'simds': 'avx', 'problem_size_x': 256, 'problem_size_y': 256,
-             'problem_size_z': 256, 'n_threads': 16, 'thrdblock_x': 16, 'thrdblock_y': 16, 'thrdblock_z': 16}
     param['olevels'] = olevels[int(x[0])]
     param['simds'] = simds[int(x[1])]
     param['n_threads'] = 8*int(x[2]+1)
@@ -26,15 +28,15 @@ def x_to_param(x):
 
 def cost_function(x):
     param = x_to_param(x)
-    print('solution :' + param['olevels'] + param['simds'] + param['problem_size_x']+param['problem_size_y'] +
-          param['problem_size_z'] + param['n_threads'] + param['thrdblock_x'] + param['thrdblock_y']+param['thrdblock_z'])
-    sol = Solution(param['olevels'], param['simds'], param['problem_size_x'], param['problem_size_y'],
-                   param['problem_size_z'], param['n_threads'], param['thrdblock_x'], param['thrdblock_y'], param['thrdblock_z'])
+    print(f'Computing of the cost of : {param}')
+    sol = Solution(param['olevels'], param['simds'], str(param['problem_size_x']), str(param['problem_size_y']),
+                   str(param['problem_size_z']), str(param['n_threads']), param['thrdblock_x'], param['thrdblock_y'], param['thrdblock_z'])
+
     return -sol.cost()
 
 
-x0 = 6 * [0]  # initial solution
-sigma0 = 1   # initial standard deviation to sample new solutions
-
-xopt, es = cma.fmin2(cost_function, x0, sigma0, {'bounds': [0, [
-                     2.99, 2.99, max_threads//8, max_thrdblock_x//8, max_thrdblock_y//8, max_thrdblock_z//8]]})
+if __name__ == "__main__":
+    x0 = 6 * [0]  # initial solution
+    sigma0 = 1   # initial standard deviation to sample new solutions
+    xopt, es = cma.fmin2(cost_function, x0, sigma0, {'bounds': [0, [
+        2.99, 2.99, max_threads//8, max_thrdblock_x//8, max_thrdblock_y//8, max_thrdblock_z//8]]})
