b4bcdd5cbbb8b96e3f25008de1016174ac32ae36
Elian Mangin <elian.mangin@student.fr>
2023-02-27 16:18:41 +0100

    bug fix

--- src/CMAES.py
+++ src/CMAES.py
@@ -2,7 +2,6 @@ import cma
 from solution import Solution
 # function to optimize
 
-# we could use `functools.partial(cma.ff.elli, cond=1e4)` to change the condition number to 1e4
 max_threads = 32
 max_thrdblock_x = 256
 max_thrdblock_y = 256
@@ -17,7 +16,7 @@ def x_to_param(x):
     param = {'olevels': '-O2', 'simds': 'avx', 'problem_size_x': 256, 'problem_size_y': 256,
              'problem_size_z': 256, 'n_threads': 16, 'thrdblock_x': 16, 'thrdblock_y': 16, 'thrdblock_z': 16}
     param['olevels'] = olevels[int(x[0])]
-    param['simds'] = olevels[int(x[1])]
+    param['simds'] = simds[int(x[1])]
     param['n_threads'] = 8*int(x[2]+1)
     param['thrdblock_x'] = 8*int(x[3]+1)
     param['thrdblock_y'] = 8*int(x[4]+1)
@@ -28,14 +27,12 @@ def x_to_param(x):
 def cost_function(x):
     param = x_to_param(x)
     sol = Solution(param['olevels'], param['simds'], param['problem_size_x'], param['problem_size_y'],
-                   param['problem_size_z'], param['n_threads'], param['thrdblock_x'], param['thrdblock_y'], param['thrdblock_y'])
-    return sol.cost()
+                   param['problem_size_z'], param['n_threads'], param['thrdblock_x'], param['thrdblock_y'], param['thrdblock_z'])
+    return -sol.cost()
 
 
-S0 = Solution('-O3', 'avx512', '256', '256',
-              '256', '16', '48', '32', '32')
 x0 = 6 * [0]  # initial solution
-sigma0 = 0.5   # initial standard deviation to sample new solutions
+sigma0 = 1   # initial standard deviation to sample new solutions
 
 xopt, es = cma.fmin2(cost_function, x0, sigma0, {'bounds': [0, [
                      2.99, 2.99, max_threads//8, max_thrdblock_x//8, max_thrdblock_y//8, max_thrdblock_z//8]]})
